1a2352445ca48e26026ef27606f0cd860c1d595e
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-02-22 15:41:29 +0900

    practice 1

--- PracticeUng/Program.cs
+++ PracticeUng/Program.cs
@@ -10,6 +10,50 @@ namespace PracticeUng
     {
         static void Main(string[] args)
         {
+            /*
+            Console.WriteLine("Test");
+
+
+            Ctrl + S: 현재 파일 저장
+            Ctrl + C: 복사
+            Ctrl + X: 잘라내기
+            Ctrl + V: 붙여넣기
+            Ctrl + Z: 실행 취소
+            Ctrl + Y: 다시 실행
+            Ctrl + F: 찾기
+            Ctrl + H: 찾기 및 바꾸기
+            Ctrl + D: 현재 선택과 일치하는 다음 항목 찾기
+            Ctrl + Shift + L: 현재 줄 삭제
+          
+            디버깅 단축키
+            F5: 디버깅 시작
+            F9: 중단점 설정/해제
+            F10: 한 단계씩 코드 실행(단계별 실행)
+            F11: 함수 호출 내부로 들어가기(단계별 실행)
+            Shift + F11: 함수 호출 빠져나오기(단계별 실행)
+            
+             
+             
+            byte a = 200;
+            sbyte b = -126;
+            short c = 30000;
+            ushort d = 40000;
+            int e = 100000000;
+            uint f = 1000000000;
+            long g = 1000000000000000000;
+            ulong h = 10000000000000000000;
+
+            Console.WriteLine("a={0}, b={1}, c={2}, 
+            d={3}, e={4}, f={5}, g={6}, h={7}", a, b, c, d, e, f, g, h);
+            char a = '안';
+            string b = "녕하세요";
+
+            Console.WriteLine("{0}{1}", a, b);
+            // 문자는 '' -> 작은따옴표
+            // 문자열은 " " -> 큰따옴표
+             
+             */
+
         }
     }
 }
