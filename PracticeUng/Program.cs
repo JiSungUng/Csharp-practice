63df96d2de9e6786231a5e7f0dc1d2f29b96ab2b
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-02-25 14:09:53 +0900

    Practice3

--- PracticeUng/Program.cs
+++ PracticeUng/Program.cs
@@ -14,7 +14,7 @@ namespace PracticeUng
              
             Console.WriteLine("Test");
 
-            https://blog.hexabrain.net/98
+            https://blog.hexabrain.net/135
 
             Ctrl + S: 현재 파일 저장
             Ctrl + C: 복사
@@ -54,8 +54,77 @@ namespace PracticeUng
             // 문자는 '' -> 작은따옴표
             // 문자열은 " " -> 큰따옴표
              
+            int a = 12345;
+            Console.WriteLine("|{0,15}|", a);       // |     12345|
+            Console.WriteLine("|{0,-15}|", a);      // |12345     |
+            Console.WriteLine("|{0,15:N0}|", a);    // |     12,345|
+            Console.WriteLine("|{0,-15:N0}|", a);   // |12,345     | 
+
+            int a = 500;
+            float b = a; // 암시적 변환으로 별도의 구문이 필요하지 않음 
+
+            Console.WriteLine("a={0}, b={1}", a, b);
+
+            double c = 123.45;
+            int d = (int)c;
+
+            Console.WriteLine("c={0}, d={1}", c, d);
+
+            int a = 500;
+            float b = 60.44f;
+            string c = a.ToString();
+            string d = b.ToString();
+
+            Console.WriteLine("a={0}, b={1}, c={2}, d={3}", a, b, c, d);
+
+            string e = "123";
+            string f = "123.456";
+            int g = int.Parse(e);
+            float h = float.Parse(f);
+
+            Console.WriteLine("e={0}, f={1}, g={2}, h={3}", e, f, g, h);
+           
+            
+            const int a = 50;
+
+            a = 60; // 오류 발생!
+
+            Console.WriteLine(a);
+            int num = 1;
+
+            do
+            {
+                Console.WriteLine("num: {0}", num++);
+            } while (num <= 10); // 마지막 세미콜론;
+
+            int[] arr = { 1, 2, 5, 7, 4, 9, 8, 10, 4, 7, 11 };
+
+            foreach (int i in arr)
+                Console.WriteLine("i: {0}", i);
+
+            int sum = 0;
+
+            while (true)
+            {
+                Console.Write("수를 입력하세요: ");
+                string line = Console.ReadLine(); // 값을 입력받는 부분!
+                if (line == "end") break; // end가 입력되면 무한 루프를 빠져나온다.
+                sum += int.Parse(line); // 문자열을 숫자로 변환한다!
+            }
+
+            Console.WriteLine("지금까지 입력된 수를 모두 더합니다: " + sum);
+
+            Console.WriteLine("{0}", Division(40, 10));
              */
 
+
+
         }
+
+        static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
     }
 }
