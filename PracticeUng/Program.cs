c7401049b401cf38c6d971117770647f7354ba3a
지성웅 <85995439+JiSungUng@users.noreply.github.com>
2024-02-29 16:33:27 +0900

    6

--- PracticeUng/Program.cs
+++ PracticeUng/Program.cs
@@ -126,6 +126,11 @@ namespace PracticeUng
             Console.WriteLine("sum={0}", total(30, 4, 5));
 
              */
+            int[,] reading = new int[3, 6] { { 4, 9, 1, 0, 21, 12 }, { 1, 4, 6, 0, 9, 5 }, { 4, 4, 2, 4, 0, 0 } };  //2 차원 배열.
+
+            for (int i = 0; i < reading.GetLength(0); i++)
+                for (int j = 0; j < reading.GetLength(1); j++)
+                    Console.WriteLine("reading[" + i + ", " + j + "] = {0}", reading[i, j]);
 
         }
 
